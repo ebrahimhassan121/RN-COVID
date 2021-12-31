4a710ca17382b4c05e1ad613965b694ffb45ec38
ebrahimhassan121 <ebrahimhassan121@gmail.com>
2022-01-01 01:23:24 +0200

    remove console log

--- src/utils/countries-summary-api.ts
+++ src/utils/countries-summary-api.ts
@@ -1,11 +1,9 @@
 import {ISummarySuccessResult} from '@src/interfaces/ISummarySuccessResult';
 import Axios from 'axios';
-let fetchCount = 1;
 export const fetchCountriesSummary =
   async (): Promise<ISummarySuccessResult> => {
     let data = await (
       await Axios.get('https://api.covid19api.com/summary')
     ).data;
-    console.log('fetch Count', fetchCount++);
     return data;
   };
